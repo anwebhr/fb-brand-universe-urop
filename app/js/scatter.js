850d4c3574b07ef6408447f2cb7451251fbadddd
anwebhr <anwebhr@users.noreply.github.com>
2017-10-12 13:33:09 +0800

    Added autocomplete search bar, zoom and filter not working yet

--- app/js/scatter.js
+++ app/js/scatter.js
@@ -15,11 +15,14 @@ var xCat = "x",
     //rCat = "Protein (g)",
     colorCat = "brand";
 
-d3.csv("app/src/fb.csv", function(data) {
-    var x_coords = [], y_coords = [];
+var brandList = [];
+var x_coords = [], y_coords = [];
+
+d3.csv("app/src/fb.csv", function(data) {    
     data.forEach(function(d) {
         x_coords.push(parseFloat(d[xCat]));
         y_coords.push(parseFloat(d[yCat]));
+        brandList.push(d[colorCat]);
         //d["x"] = +d["x"];
         //d["y"] = +d["y"];
         /*d.Calories = +d.Calories;
@@ -35,6 +38,7 @@ d3.csv("app/src/fb.csv", function(data) {
         d.Sugars = +d.Sugars;
         d["Vitamins and Minerals"] = +d["Vitamins and Minerals"]; */
     });
+
     var xMax = x_coords.sort(function(a,b) {return a-b})[x_coords.length-1],
         xMin = x_coords.sort(function(a,b) {return a-b})[0],
         yMax = y_coords.sort(function(a,b) {return a-b})[y_coords.length-1],
@@ -46,10 +50,10 @@ d3.csv("app/src/fb.csv", function(data) {
         yMin = d3.min(data, function(d) { return d[yCat]; });
         yMin = yMin > 0 ? 0 : yMin; */
 
-    console.log("xmax: ", xMax);
-    console.log("xmin: ", xMin);
-    console.log("ymax: ", yMax);
-    console.log("ymin: ", yMin);
+    //console.log("xmax: ", xMax);
+    //console.log("xmin: ", xMin);
+    //console.log("ymax: ", yMax);
+    //console.log("ymin: ", yMin);
 
     x.domain([xMin, xMax]);
     y.domain([yMin, yMax]);
@@ -95,10 +99,10 @@ d3.csv("app/src/fb.csv", function(data) {
 
     svg.append("g")
         .classed("x axis", true)
-        .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
         .append("text")
         .classed("label", true)
+        .attr("transform", "translate(0," + height + ")")
         .attr("x", width)
         .attr("y", margin.bottom - 10)
         //.style("text-anchor", "end")
@@ -162,7 +166,7 @@ d3.csv("app/src/fb.csv", function(data) {
         .attr("dy", ".35em")
         .text(function(d) { return d; }); */
 
-    d3.select("input").on("click", change);
+    //d3.select("input").on("click", change);
 
     function change() {
         xCat = "x";
@@ -189,4 +193,52 @@ d3.csv("app/src/fb.csv", function(data) {
     function transform(d) {
         return "translate(" + x(d[xCat]) + "," + y(d[yCat]) + ")";
     }
+
+    
+    $('#searchOptions').autocomplete({
+        source: brandList.sort()
+    });
+    
+    $("#searchOptions").keyup(function (e) {
+        if (e.keyCode == 13) {
+            console.log('pressed enter');
+            searchBrand();
+        }
+    });
+    //var svg = d3.select("body").append("svg")
+    //.attr("width", width)
+    //.attr("height", height);
+
+    d3.selectAll("button").on("click", searchBrand);
+
+    function searchBrand() {
+        //var selectedBrand = document.getElementById('searchOptions').value;
+        //var svg = d3.select("#scatter").transition();
+         //var svg = d3.select("#scatter").append("svg")
+        //.attr("width", width)
+        // .attr("height", height);
+        //    svg.select(".x.axis").duration(750).call(xAxis).select(".label").text(xCat);
+        //    objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
+        //var selectedBrand = $('#searchOptions').val();
+        var selectedBrand = this.value;
+        console.log(selectedBrand);
+        var node = svg.selectAll(".dot");
+        if (selectedBrand == "none") {
+            node.style("stroke", "white").style("stroke-width", "1");
+        } else {
+            var selected = node.filter(function (d, i) {
+                return d.name != selectedBrand;
+            });
+            selected.style("opacity", "0");
+            var link = svg.selectAll(".link")
+            link.style("opacity", "0");
+            d3.selectAll(".node, .link").transition()
+                .duration(5000)
+                .style("opacity", 1);
+        }
+    }
+    
 });
+
+
+
