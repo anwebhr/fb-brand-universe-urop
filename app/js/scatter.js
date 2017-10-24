e6843af89a79028329f404de24c5b7c8d0936966
anwebhr <anwebhr@users.noreply.github.com>
2017-10-24 18:14:41 +0800

    alert with coordinates info generated, redesigned search panel

--- app/js/scatter.js
+++ app/js/scatter.js
@@ -23,37 +23,12 @@ d3.csv("app/src/fb.csv", function(data) {
         x_coords.push(parseFloat(d[xCat]));
         y_coords.push(parseFloat(d[yCat]));
         brandList.push(d[colorCat]);
-        //d["x"] = +d["x"];
-        //d["y"] = +d["y"];
-        /*d.Calories = +d.Calories;
-        d.Carbs = +d.Carbs;
-        d["Cups per Serving"] = +d["Cups per Serving"];
-        d["Dietary Fiber"] = +d["Dietary Fiber"];
-        d["Display Shelf"] = +d["Display Shelf"];
-        d.Fat = +d.Fat;
-        d.Potassium = +d.Potassium;
-        d["Protein (g)"] = +d["Protein (g)"];
-        d["Serving Size Weight"] = +d["Serving Size Weight"];
-        d.Sodium = +d.Sodium;
-        d.Sugars = +d.Sugars;
-        d["Vitamins and Minerals"] = +d["Vitamins and Minerals"]; */
     });
 
     var xMax = x_coords.sort(function(a,b) {return a-b})[x_coords.length-1],
         xMin = x_coords.sort(function(a,b) {return a-b})[0],
         yMax = y_coords.sort(function(a,b) {return a-b})[y_coords.length-1],
         yMin = y_coords.sort(function(a,b) {return a-b})[0];
-    /*var xMax = d3.max(data, function(d) { return d[xCat]; }), //* 1.05,
-        xMin = d3.min(data, function(d) { return d[xCat]; }),
-        xMin = xMin > 0 ? 0 : xMin,
-        yMax = d3.max(data, function(d) { return d[yCat]; }), //* 1.05,
-        yMin = d3.min(data, function(d) { return d[yCat]; });
-        yMin = yMin > 0 ? 0 : yMin; */
-
-    //console.log("xmax: ", xMax);
-    //console.log("xmin: ", xMin);
-    //console.log("ymax: ", yMax);
-    //console.log("ymin: ", yMin);
 
     x.domain([xMin, xMax]);
     y.domain([yMin, yMax]);
@@ -105,8 +80,6 @@ d3.csv("app/src/fb.csv", function(data) {
         .attr("transform", "translate(0," + height + ")")
         .attr("x", width)
         .attr("y", margin.bottom - 10)
-        //.style("text-anchor", "end")
-        //.text(xCat);
 
     svg.append("g")
         .classed("y axis", true)
@@ -116,8 +89,6 @@ d3.csv("app/src/fb.csv", function(data) {
         .attr("transform", "rotate(-90)")
         .attr("y", -margin.left)
         .attr("dy", ".71em")
-        //.style("text-anchor", "end")
-        //.text(yCat);
 
     var objects = svg.append("svg")
         .classed("objects", true)
@@ -143,7 +114,6 @@ d3.csv("app/src/fb.csv", function(data) {
         .data(data)
         .enter().append("circle")
         .classed("dot", true)
-        //.attr("r", function (d) { return 6 * Math.sqrt(d[rCat] / Math.PI); })
         .attr("r", 4)
         .attr("transform", transform)
         .style("fill", function(d) { return color(d[colorCat]); })
@@ -157,29 +127,18 @@ d3.csv("app/src/fb.csv", function(data) {
         .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
     legend.append("circle")
-        //.attr("r", 3.5)
         .attr("cx", width + 20)
         .attr("fill", color);
-
-    /*legend.append("text")
-        .attr("x", width + 26)
-        .attr("dy", ".35em")
-        .text(function(d) { return d; }); */
-
-    //d3.select("input").on("click", change);
-
+    
+    
     function change() {
         xCat = "x";
         xMax = d3.max(data, function(d) { return d[xCat]; });
         xMin = d3.min(data, function(d) { return d[xCat]; });
-
-    zoomBeh.x(x.domain([xMin, xMax])).y(y.domain([yMin, yMax]));
-
-    var svg = d3.select("#scatter").transition();
-
-    svg.select(".x.axis").duration(750).call(xAxis).select(".label").text(xCat);
-
-    objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
+        zoomBeh.x(x.domain([xMin, xMax])).y(y.domain([yMin, yMax]));
+        var svg = d3.select("#scatter").transition();
+        svg.select(".x.axis").duration(750).call(xAxis).select(".label").text(xCat);
+        objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
     }
 
     function zoom() {
@@ -194,47 +153,43 @@ d3.csv("app/src/fb.csv", function(data) {
         return "translate(" + x(d[xCat]) + "," + y(d[yCat]) + ")";
     }
 
+    var options_autocomplete = {
+        data: brandList.sort(),
+        list: {
+            match: {
+                enabled: true
+            }
+        },
+        theme: "blue-light"
+    };
     
-    $('#searchOptions').autocomplete({
-        source: brandList.sort()
-    });
+    $("#searchOptions").easyAutocomplete(options_autocomplete);
     
-    $("#searchOptions").keyup(function (e) {
+    $('#searchOptions').keyup(function (e) {
         if (e.keyCode == 13) {
             console.log('pressed enter');
             searchBrand();
         }
     });
-    //var svg = d3.select("body").append("svg")
-    //.attr("width", width)
-    //.attr("height", height);
 
-    d3.selectAll("button").on("click", searchBrand);
+    d3.select("button").on("click", searchBrand);
 
     function searchBrand() {
-        //var selectedBrand = document.getElementById('searchOptions').value;
-        //var svg = d3.select("#scatter").transition();
-         //var svg = d3.select("#scatter").append("svg")
-        //.attr("width", width)
-        // .attr("height", height);
-        //    svg.select(".x.axis").duration(750).call(xAxis).select(".label").text(xCat);
-        //    objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
-        //var selectedBrand = $('#searchOptions').val();
-        var selectedBrand = this.value;
-        console.log(selectedBrand);
-        var node = svg.selectAll(".dot");
-        if (selectedBrand == "none") {
-            node.style("stroke", "white").style("stroke-width", "1");
-        } else {
-            var selected = node.filter(function (d, i) {
-                return d.name != selectedBrand;
-            });
-            selected.style("opacity", "0");
-            var link = svg.selectAll(".link")
-            link.style("opacity", "0");
-            d3.selectAll(".node, .link").transition()
-                .duration(5000)
-                .style("opacity", 1);
+    
+        var selectedBrand = $('#searchOptions').val();
+        var index = -1;
+        for (var i=0; i<brandList.length; i++) {
+            if(brandList[i] === selectedBrand){
+                index = i;
+            }
+        }
+        if(index != -1){
+            alert("Brand Name: "+ selectedBrand + "\n" +
+            "X Coordinate: "+ x_coords[index] + "\n"+
+            "Y Coordinate: "+ y_coords[index] );
+        }
+        else{
+            alert("Brand "+ selectedBrand + " not found");
         }
     }
     
